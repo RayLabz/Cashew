b5024c02d73a4181d63b66edcdda8779661c49fe
Nicos <hfnovember@hotmail.com>
2020-09-14 23:20:39 +0300

    Fixed exception access modifiers.

--- src/main/java/com/raylabz/cashew/exception/NoCacheException.java
+++ src/main/java/com/raylabz/cashew/exception/NoCacheException.java
@@ -9,11 +9,11 @@ public class NoCacheException extends Exception {
      * Constructs a NoCacheException.
      * @param aClass The class of the cache.
      */
-    NoCacheException(final Class<?> aClass) {
+    public NoCacheException(final Class<?> aClass) {
         super("No cache found for class '" + aClass.getSimpleName() + "'. Use Cashew.registerClass() to create a new cache for this class.");
     }
 
-    NoCacheException(final String cacheName) {
+    public NoCacheException(final String cacheName) {
         super("No cache found for name '" + cacheName + "'. Use Cashew.createCache() to create a new cache.");
     }
 
