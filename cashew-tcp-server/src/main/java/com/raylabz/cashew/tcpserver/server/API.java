543eac5e1f6ced9d87cd3616a708c66e75e05342
Nicos <hfnovember@hotmail.com>
2020-09-15 13:22:23 +0300

    Working Add, Get for server.

--- cashew-tcp-server/src/main/java/com/raylabz/cashew/tcpserver/server/API.java
+++ cashew-tcp-server/src/main/java/com/raylabz/cashew/tcpserver/server/API.java
@@ -1,9 +1,6 @@
 package com.raylabz.cashew.tcpserver.server;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import com.raylabz.cashew.CacheItem;
 import com.raylabz.cashew.Cashew;
 import com.raylabz.cashew.StringCache;
@@ -34,24 +31,16 @@ public class API {
                         final String objectClass = params.getString("objectClass");
                         try {
                             StringCache<String> cache = Cashew.getCache(objectClass);
-                            if (cache.add(key, value)) {
-                                return new SuccessResponse("Value added", "The value has been added.");
-                            }
-                            else {
-                                return new ErrorResponse("Failed", "Could not add value.");
-                            }
+                            cache.add(key, value);
+                            return new SuccessResponse("Value added", "The value has been added.");
                         } catch (NoCacheException e) {
                             return new ErrorResponse("No cache", "The cache '" + objectClass + "' does not exist.");
                         }
                     }
                     else {
                         StringCache<Object> cache = Cashew.getMainCache();
-                        if (cache.add(key, value)) {
-                            return new SuccessResponse("Value added", "The value has been added.");
-                        }
-                        else {
-                            return new ErrorResponse("Failed", "Could not add value.");
-                        }
+                        cache.add(key, value);
+                        return new SuccessResponse("Value added", "The value has been added.");
                     }
                 }
             })
@@ -69,24 +58,16 @@ public class API {
                         final String objectClass = params.getString("objectClass");
                         try {
                             StringCache<String> cache = Cashew.getCache(objectClass);
-                            if (cache.delete(key)) {
-                                return new SuccessResponse("Value deleted", "The value has been deleted.");
-                            }
-                            else {
-                                return new ErrorResponse("Failed", "Could not delete value.");
-                            }
+                            cache.delete(key);
+                            return new SuccessResponse("Value deleted", "The value has been deleted.");
                         } catch (NoCacheException e) {
                             return new ErrorResponse("No cache", "The cache '" + objectClass + "' does not exist.");
                         }
                     }
                     else {
                         StringCache<Object> cache = Cashew.getMainCache();
-                        if (cache.delete(key)) {
-                            return new SuccessResponse("Value deleted", "The value has been deleted.");
-                        }
-                        else {
-                            return new ErrorResponse("Failed", "Could not delete value.");
-                        }
+                        cache.delete(key);
+                        return new SuccessResponse("Value deleted", "The value has been deleted.");
                     }
 
                 }
@@ -107,24 +88,16 @@ public class API {
                         final String objectClass = params.getString("objectClass");
                         try {
                             StringCache<String> cache = Cashew.getCache(objectClass);
-                            if (cache.update(key, value)) {
-                                return new SuccessResponse("Value updated", "The value has been updated.");
-                            }
-                            else {
-                                return new ErrorResponse("Failed", "Could not update value.");
-                            }
+                            cache.update(key, value);
+                            return new SuccessResponse("Value updated", "The value has been updated.");
                         } catch (NoCacheException e) {
                             return new ErrorResponse("No cache", "The cache '" + objectClass + "' does not exist.");
                         }
                     }
                     else {
                         StringCache<Object> cache = Cashew.getMainCache();
-                        if (cache.update(key, value)) {
-                            return new SuccessResponse("Value updated", "The value has been updated.");
-                        }
-                        else {
-                            return new ErrorResponse("Failed", "Could not update value.");
-                        }
+                        cache.update(key, value);
+                        return new SuccessResponse("Value updated", "The value has been updated.");
                     }
 
                 }
@@ -162,10 +135,25 @@ public class API {
                         final Object value = cache.get(key);
                         if (value != null) {
                             System.out.println("value: " + value);
-                            JsonElement valueElement = new Gson().fromJson(value.toString(), JsonObject.class);
-                            JsonObject data = new JsonObject();
-                            data.add("value", valueElement);
-                            return new SuccessResponse("Value fetched", "The value was fetched", data);
+                            JsonElement valueElement = null;
+                            try {
+                                valueElement = new Gson().fromJson(value.toString(), JsonObject.class);
+                            } catch (JsonSyntaxException e) {
+                                try {
+                                    valueElement = new Gson().fromJson(value.toString(), JsonPrimitive.class);
+                                } catch (JsonSyntaxException jsonSyntaxException) {
+                                    jsonSyntaxException.printStackTrace();
+                                }
+                            }
+
+                            if (valueElement != null) {
+                                JsonObject data = new JsonObject();
+                                data.add("value", valueElement);
+                                return new SuccessResponse("Value fetched", "The value was fetched", data);
+                            }
+                            else {
+                                return new ErrorResponse("Failed", "Could not fetch value. Invalid data type.");
+                            }
                         }
                         else {
                             return new ErrorResponse("Failed", "Could not fetch value.");
--- cashew-tcp-server/src/test/java/TestClient.java
+++ cashew-tcp-server/src/test/java/TestClient.java
@@ -1,12 +1,59 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.raylabz.cashew.tcpserver.client.CashewTCPClient;
+import com.raylabz.cashew.tcpserver.server.CashewTCPServer;
+import com.raylabz.cashew.tcpserver.server.request.AddRequest;
+import com.raylabz.cashew.tcpserver.server.request.GetRequest;
+import com.raylabz.mocha.client.TCPClient;
 import com.raylabz.mocha.server.Mocha;
 
 import java.io.IOException;
 
-public class TestClient {
+public class TestClient extends CashewTCPClient {
+
+    public TestClient(String ipAddress) throws IOException {
+        super(ipAddress);
+    }
+
+    @Override
+    public void onConnectionRefused() {
+        System.err.println("Connection refused.");
+    }
+
+    @Override
+    public void initialize() {
+        Person person = new Person("Nicos", "Kasenides");
+        final JsonElement jsonElement = new Gson().toJsonTree(person);
+        AddRequest addRequest = new AddRequest("key1", new Gson().toJson(jsonElement), null);
+        String sending = new Gson().toJson(addRequest);
+        System.out.println("Sending: " + sending);
+        send(sending);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        GetRequest getRequest = new GetRequest("key1", null);
+        sending = new Gson().toJson(getRequest);
+        System.out.println("Sending: " + sending);
+        send(sending);
+
+    }
+
+    @Override
+    public void process() {
+
+    }
+
+    @Override
+    public void onReceive(String data) {
+        System.out.println("Got: " + data);
+    }
 
     public static void main(String[] args) throws IOException {
-        final CashewTCPClient client = new CashewTCPClient("localhost");;
+        final TestClient client = new TestClient("localhost");
         Mocha.start(client);
     }
 
